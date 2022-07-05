899ba8bda2a0e2a1e60e047e291855f071bb798e
avadecer <avadecer@gmail.com>
2022-07-05 19:51:55 +0800

    sign in

--- ECOQuizAdmin/app/src/main/java/com/example/ecoquizadmin/MainActivity.java
+++ ECOQuizAdmin/app/src/main/java/com/example/ecoquizadmin/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.ecoquizadmin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     private EditText email, pass;
     private Button login;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
         email = findViewById(R.id.email);
         pass = findViewById(R.id.password);
         login = findViewById(R.id.loginBtn);
+
+        firebaseAuth = FirebaseAuth.getInstance();
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,7 +55,19 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void firebaseLogin(){
-        
+    private void firebaseLogin() {
+        firebaseAuth.signInWithEmailAndPassword(email.getText().toString(), pass.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(MainActivity.this, "Failure", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
-}
+    }
